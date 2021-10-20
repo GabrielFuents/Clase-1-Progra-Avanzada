5356f9358d8cc18df7ee77c95d0cd706beea0356
GABRIEL FUENTES <gfuentes50767@ufide.ac.cr>
2021-10-20 13:15:39 -0600

    'Semana 2'

---
+++ Practica2/Data/Practica2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Practica2.Models;
+
+namespace Practica2.Data
+{
+    public class Practica2Context : DbContext
+    {
+        public Practica2Context (DbContextOptions<Practica2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Practica2.Models.Comida> Comida { get; set; }
+    }
+}
---
+++ Practica2/Models/Comida.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica2.Models
+{
+    public class Comida
+    {
+        public int Id { get; set; }
+
+        public int HoraConsumo { get; set; }
+
+        public int Consumo { get; set; }
+
+        public int Total { get; set; }
+
+        public int Local { get; set; }
+
+        public int Empleado { get; set; }
+
+
+    }
+}
